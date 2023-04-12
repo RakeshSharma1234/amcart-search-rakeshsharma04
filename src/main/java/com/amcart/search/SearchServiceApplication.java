851114b2d9a547d0ac2fa08ca6c087534a02adcd
Rakesh Sharma <you@example.comcls>
2023-04-12 11:16:25 +0530

    Changes

--- src/main/java/com/amcart/search/SearchServiceApplication.java
+++ src/main/java/com/amcart/search/SearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.amcart.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.amcart"})
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
